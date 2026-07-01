0c98f0106cfbc0150ab2063f6da3328196ceaaec
Shivansh-Singh-07 <shivansh.singh.cs28@iilm.edu>
2026-07-02 01:09:28 +0530

    committtt

---
+++ firebase-config.js
@@ -0,0 +1,18 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyBTOuOvv6T8bSVmUFcYwFfXBc-TN56zQS0",
+  authDomain: "private-listening-party.firebaseapp.com",
+  projectId: "private-listening-party",
+  storageBucket: "private-listening-party.firebasestorage.app",
+  messagingSenderId: "577600648946",
+  appId: "1:577600648946:web:c40fffcc09a1115419c26b",
+  measurementId: "G-6RZHDBJ8MR"
+};
+
+export const EVENT_CONFIG = {
+  MAX_GUESTS: 120,
+  MAX_PARTY_SIZE: 4,
+  EVENT_DATE: "2026-08-15T20:30:00+05:30",
+  EVENT_LOCATION: "Revealed when approved",
+  ARRIVAL_TIME: "Saturday, 8:30 PM",
+  DRESS_CODE: "Black, silver, something you can move in"
+};
--- js/background-images.js
+++ js/background-images.js
@@ -1,4 +1,4 @@
-export const BACKGROUND_IMAGES = {
+﻿export const BACKGROUND_IMAGES = {
   hero: "assets/hero.jpg",
   confirmation: "assets/confirmation.jpg",
   approved: "assets/approved.jpg",
@@ -6,8 +6,12 @@ export const BACKGROUND_IMAGES = {
   crowd: "assets/crowd.jpg"
 };
 
+function assetUrl(path) {
+  return new URL(path, document.baseURI).href;
+}
+
 document.querySelectorAll("[data-bg-key]").forEach((node) => {
   const key = node.dataset.bgKey;
   const url = BACKGROUND_IMAGES[key];
-  if (url) node.style.setProperty("--bg-image", `url("${url}")`);
+  if (url) node.style.setProperty("--bg-image", `url("${assetUrl(url)}")`);
 });
